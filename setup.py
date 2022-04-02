71bf02399c925b6216e66e9c6023409e54f29761
Nikolai Limbrunner <ntli@kth.se>
2022-04-02 17:57:37 +0200

    Increase version to prevent PyPi error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="uselesspackage",
-    version="0.0.1",
+    version="0.0.2",
     author="Nikolai Limbrunner",
     author_email="nikolai.limbrunner@gmail.com",
     description="Useless package for demo purposes",
